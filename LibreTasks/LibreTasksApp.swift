e162c5714bb2563b0ce154050b13295788577dce
Christian Cleberg <hello@cleberg.io>
2022-04-12 20:27:41 -0500

    Initial Commit

---
+++ LibreTasks/LibreTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibreTasksApp.swift
+//  LibreTasks
+//
+//  Created by kaizoku on 4/12/22.
+//
+
+import SwiftUI
+
+@main
+struct LibreTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
